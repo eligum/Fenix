6f2a6cfc648cdb96e997e35358aa259a516a81fd
Miguel García Soler <miguelgarciasoler0@gmail.com>
2021-02-18 20:59:14 +0100

    Some stuff

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -4,3 +4,19 @@ function(print)
         message("${var} = ${${var}}")
     endforeach()
 endfunction()
+
+# Aborts with fatal error if user tried to create an in-source cmake build
+function(PreventInSourceBuilds)
+    # make sure the user doesn't play dirty with symlinks
+    get_filename_component(srcdir "${CMAKE_SOURCE_DIR}" REALPATH)
+    get_filename_component(bindir "${CMAKE_BINARY_DIR}" REALPATH)
+
+    # disallow in-source builds
+    if("${srcdir}" STREQUAL "${bindir}")
+        message("ERROR! You should not configure this project with an in-source build.")
+        message("NOTE: Given that you already tried to make an in-source build, CMake")
+        message("      has already created several files and directories in your source")
+        message("      tree. Run 'git status' to find them and remove them.")
+        message(FATAL_ERROR)
+    endif()
+endfunction()
