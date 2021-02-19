cbedaa35ff99e5208503d979cea4cfc00b0655ae
Miguel García Soler <miguelgarciasoler0@gmail.com>
2021-02-19 15:37:53 +0100

    Finally everything works!!

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "glm/glm.hpp"
+#include "GLFW/glfw3.h"
 
 int main()
 {
+    glm::vec3 light(1.0f);
+    std::cout << light.x << std::endl;
     std::cout << "Hello World!" << std::endl;
 }
