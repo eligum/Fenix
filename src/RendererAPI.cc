160b50e7fc42beb8f53379c95b5e1854dd39861f
Miguel García Soler <miguelgarciasoler0@gmail.com>
2021-03-11 13:32:57 +0100

    fixed linking error

--- src/RendererAPI.cc
+++ src/RendererAPI.cc
@@ -3,6 +3,6 @@
 namespace Hazel {
 
     // TEMP: In the future this should be set at runtime
-    RendererAPI::API s_API = RendererAPI::API::OpenGL;
+    RendererAPI::API RendererAPI::s_API = RendererAPI::API::OpenGL;
 
 } // namespace Hazel
